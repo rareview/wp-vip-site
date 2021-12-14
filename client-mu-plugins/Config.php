8e7a982a075c2a4f449f24b1837ea59245c2da74
Justin Kopepasah <justin@kopepasah.com>
2021-12-14 03:52:47 -0600

    Update config.

--- client-mu-plugins/Config.php
+++ client-mu-plugins/Config.php
@@ -19,33 +19,44 @@ abstract class Config {
 	/**
 	 * Gets the plugin directory path.
 	 *
+	 * @param string $file File to retireve.
+	 *
 	 * @return string Plugin dir path.
 	 */
-	public static function dir() {
-		return plugin_dir_path( __FILE__ );
-	}
+	public static function dir( $file = '' ) {
+		if ( substr( $file, 0, 17 ) === 'client-mu-plugins' ) {
+			$file = str_replace( 'client-mu-plugins/', '', $file );
+		}
 
-	/**
-	 * Gets the plugin file path.
-	 *
-	 * @return string Plugin file.
-	 */
-	public static function file() {
-		return __FILE__;
+		return WP_CONTENT_DIR . '/client-mu-plugins/' . $file;
 	}
 
 	/**
 	 * Gets the plugin url.
 	 *
+	 * @param string $file File to retireve.
+	 * @param bool   $append_client Whether to append client-mu-plugins.
+	 *
 	 * @return string Plugin url.
 	 */
-	public static function url() {
-		return plugin_dir_url( __FILE__ );
+	public static function url( $file = '', $append_client = true ) {
+		$url = WP_CONTENT_URL;
+
+		if ( $append_client ) {
+			$url .= '/client-mu-plugins/';
+		}
+
+		return trailingslashit( $url ) . $file;
 	}
 }
 
 // Load up the party supplies.
 require_once __DIR__ . '/vendor/autoload.php';
 
-// 🥳 Let's get this party started.
-new Plugin\Loader();
+// Load the plugins early.
+\add_action(
+	'muplugins_loaded',
+	function() {
+		new PluginName\Plugin();
+	}
+);
